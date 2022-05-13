9ca238fbdcec6928784724bd3fa92e94725d9242
Dixon <zdixon91.zd@gmail.com>
2022-05-12 21:05:20 -0500

    intial commit

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+import ScoreboardView from "./scoreboard";
+
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let playerThreeScore = 0;
+let playerFourScore = 0;
+
+const root = document.querySelector("#app");
+const view = new ScoreboardView(root, "Player One", "Player Two", (player, direction) => {
+	const difference = direction === "minus" ? -1 : 1;
+
+	if (player === "one") {
+		playerOneScore = Math.max(playerOneScore + difference, 0);
+	} else {
+		playerTwoScore = Math.max(playerTwoScore + difference, 0);
+	}
+
+	view.update(playerOneScore, playerTwoScore);
+});
